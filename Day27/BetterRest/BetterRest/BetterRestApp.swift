779473cb3d43d574491f8c691a3a7ecf5e51d1e0
Akash Bhardwaj <bhardwajakash948@gmail.com>
2023-08-21 17:38:07 -0400

    day 28

---
+++ Day27/BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Akash Bhardwaj on 2023-08-16.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
