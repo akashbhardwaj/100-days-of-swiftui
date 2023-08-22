3641b57e44fafcb70b3caf045b88f23c21134de9
Akash Bhardwaj <bhardwajakash948@gmail.com>
2023-08-22 19:02:09 -0400

    Day29

--- Day29/WordScramble/WordScramble/ContentView.swift
+++ Day29/WordScramble/WordScramble/ContentView.swift
@@ -8,14 +8,65 @@
 import SwiftUI
 
 struct ContentView: View {
+    let people = ["Finn", "Leia", "Luke"]
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        listExample
+    }
+
+    var listExample: some View {
+        List {
+            Section("Section 1") {
+                Text("Hello Worlds")
+                Text("Hello Worlds")
+            }
+            Section("Section 2") {
+                ForEach(people, id: \.self) {
+                    Text($0)
+                }
+            }
+            Section("Section 3") {
+                Text("Bye Worlds")
+                Text("Bye Worlds")
+            }
         }
-        .padding()
+        .listStyle(.grouped)
+    }
+
+    var loadingResources: some View {
+        Text("sd")
+    }
+
+    func loadFile() {
+        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
+            if let fileContent = try? String(contentsOf: fileURL) {
+                print(fileContent)
+            }
+        }
+    }
+
+
+    func strings () {
+        let input = """
+a
+b
+c
+"""
+        let letters = input.components(separatedBy: .newlines)
+
+    }
+
+    func spelling() {
+        let word = "swift"
+        let checker = UITextChecker()
+
+        let range = NSRange(location: 0, length: word.utf16.count)
+
+        let misspelledRange = checker.rangeOfMisspelledWord(in: word, range: range, startingAt: 0, wrap: false, language: "en")
+
+
+        let allGood = misspelledRange.location == NSNotFound
+
+
     }
 }
 
