29d7bb32dfbd16d171570889c58887ae501bba20
Akash Bhardwaj <bhardwajakash948@gmail.com>
2023-08-22 19:01:31 -0400

    Day29

---
+++ Day29/WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Akash Bhardwaj on 2023-08-22.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
