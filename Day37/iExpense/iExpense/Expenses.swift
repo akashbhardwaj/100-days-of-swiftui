450dcf3b9547595fec76856827765b5f7d4dd905
Akash Bhardwaj <bhardwajakash948@gmail.com>
2023-09-11 17:49:35 -0400

    Day37

---
+++ Day37/iExpense/iExpense/Expenses.swift
@@ -0,0 +1,29 @@
+//
+//  Expenses.swift
+//  iExpense
+//
+//  Created by Akash Bhardwaj on 2023-09-11.
+//
+
+import Foundation
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]() {
+        didSet {
+            let encoder = JSONEncoder()
+            if let encoded = try? encoder.encode(items) {
+                UserDefaults.standard.set(encoded, forKey: "items")
+            }
+        }
+    }
+
+    init() {
+        if let savedItems = UserDefaults.standard.data(forKey: "items") {
+            if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
+                items = decodedItems
+                return
+            }
+        }
+        items = []
+    }
+}
