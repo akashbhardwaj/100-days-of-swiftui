82af75472765413c050f48432930d02faa44a4dc
Akash Bhardwaj <bhardwajakash948@gmail.com>
2023-10-11 16:35:14 -0400

    Day50

---
+++ Day49/CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Akash Bhardwaj on 2023-10-06.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
