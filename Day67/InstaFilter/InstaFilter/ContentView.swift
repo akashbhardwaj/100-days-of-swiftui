2765cd735e0dfe3c4bcf4e6505972ef9c1042ebb
Akash Bhardwaj <bhardwajakash948@gmail.com>
2023-11-16 15:18:27 -0500

    Day67

---
+++ Day67/InstaFilter/InstaFilter/ContentView.swift
@@ -0,0 +1,146 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by Akash Bhardwaj on 2023-11-02.
+//
+
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import SwiftUI
+
+struct ContentView: View {
+    @State private var image: Image?
+    @State private var filterIntensity = 0.5
+    @State private var filterRadius = 0.5
+    @State private var showingImagePicker = false
+    @State private var inputImage: UIImage?
+    @State private var showingFilterSheet = false
+
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    @State private var processedImage: UIImage?
+    let context = CIContext()
+
+    var body: some View {
+        NavigationView {
+            VStack {
+                ZStack {
+                    Rectangle()
+                        .fill(.secondary)
+
+                    Text("Tap to select a picture")
+                        .foregroundStyle(.white)
+                        .font(.headline)
+
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                }
+                .onTapGesture(perform: onTapGesture)
+
+                VStack {
+                    if currentFilter.inputKeys.contains(kCIInputIntensityKey) {
+                        Text("Intensity")
+                        Slider(value: $filterIntensity)
+                            .onChange(of: filterIntensity) { _, _ in applyProcessing() }
+                    }
+                    if currentFilter.inputKeys.contains(kCIInputRadiusKey) {
+                        Text("Radius")
+                        Slider(value: $filterRadius)
+                            .onChange(of: filterRadius) { _, _ in
+                                applyProcessing()
+                            }
+                    }
+                }
+                .padding(.vertical)
+
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    Spacer()
+                    Button("Save", action: save)
+                        .disabled(inputImage == nil)
+                }
+            }
+            .padding([.horizontal, .bottom])
+            .navigationTitle("InstaFilter")
+            .onChange(of: inputImage) { _, _ in loadImage() }
+            .sheet(isPresented: $showingImagePicker) {
+                ImagePicker(image: $inputImage)
+            }
+            .confirmationDialog("Select the filter", isPresented: $showingFilterSheet) {
+                Button("Crystallize") {setFilter(.crystallize()) }
+                Button("Edges") { setFilter(.edges()) }
+                Button("Gaussian Blur") { setFilter(.gaussianBlur()) }
+                Button("Pixellate") { setFilter(.pixellate()) }
+                Button("Sepia Tone") { setFilter(.sepiaTone()) }
+                Button("Unsharp Mask") { setFilter(.unsharpMask()) }
+                Button("Vignette") { setFilter(.vignette()) }
+                Button("Sharpen Luminance") { setFilter(.sharpenLuminance())}
+                Button("Dot screen") { setFilter(.dotScreen())}
+                Button("False Color") { setFilter(.falseColor())}
+                Button("Cancel", role: .cancel) { }
+            }
+        }
+    }
+
+    func loadImage() {
+        guard let inputImage else { return }
+
+        let beginImage = CIImage(image: inputImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+
+    func save() {
+        guard let processedImage else { return }
+        let imageSaver = ImageSaver {
+            print("Success")
+        } errorHandler: { error in
+            print("Error: \(error.localizedDescription)")
+        }
+
+        imageSaver.writeToPhotoAlbum(image: processedImage)
+    }
+
+    func changeFilter() {
+        showingFilterSheet = true
+    }
+
+    func onTapGesture() {
+        showingImagePicker = true
+    }
+
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(filterRadius * 200, forKey: kCIInputRadiusKey)
+        }
+
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+        }
+
+
+        guard let outputImage = currentFilter.outputImage else { return }
+
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            processedImage = uiImage
+            image = Image(uiImage: uiImage)
+        }
+    }
+
+    func setFilter(_ filter: CIFilter) {
+        currentFilter = filter
+        loadImage()
+    }
+}
+
+#Preview {
+    ContentView()
+}
