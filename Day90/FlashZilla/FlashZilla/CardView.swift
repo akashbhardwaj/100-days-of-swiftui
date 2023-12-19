c2f6c674602ad595a352a098c44cba1b48475e1d
Akash Bhardwaj <bhardwajakash948@gmail.com>
2023-12-19 17:26:06 -0500

    Day90

---
+++ Day90/FlashZilla/FlashZilla/CardView.swift
@@ -0,0 +1,84 @@
+//
+//  CardView.swift
+//  FlashZilla
+//
+//  Created by Akash Bhardwaj on 2023-12-18.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    let card: Card
+    var onRemove: (() -> Void)? = nil
+    @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+    @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                .fill(
+                    differentiateWithoutColor
+                    ? .white
+                    : .white.opacity(1 - Double(abs(offset.width / 50)))
+
+                )
+                .background(
+                    differentiateWithoutColor
+                    ? nil
+                    : RoundedRectangle(cornerRadius: 25.0, style: .continuous)
+                        .fill(offset.width > 0 ? .green : .red)
+                )
+                .shadow(radius: 10)
+
+            VStack {
+                if voiceOverEnabled {
+                    Text(isShowingAnswer
+                         ? card.answer
+                         : card.prompt)
+                        .font(.largeTitle)
+                        .foregroundStyle(.black)
+                } else {
+                    Text(card.prompt)
+                        .font(.largeTitle)
+                        .foregroundStyle(.black)
+
+                    if isShowingAnswer {
+                        Text(card.answer)
+                            .font(.title)
+                            .foregroundStyle(.gray)
+                    }
+                }
+            }
+            .padding()
+            .multilineTextAlignment(.center)
+        }
+        .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(Double(offset.width / 5)))
+        .offset(x: offset.width * 5, y: 0)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .accessibilityAddTraits(.isButton)
+        .gesture(
+            DragGesture()
+                .onChanged { gesture in
+                    offset = gesture.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        onRemove?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
+        .onTapGesture {
+            isShowingAnswer.toggle()
+        }
+        .animation(.spring(), value: offset)
+    }
+}
+
+#Preview {
+    CardView(card: Card.example)
+}
---
+++ Day90/FlashZilla/FlashZilla/ContentView.swift
@@ -0,0 +1,168 @@
+//
+//  ContentView.swift
+//  FlashZilla
+//
+//  Created by Akash Bhardwaj on 2023-12-17.
+//
+
+import SwiftUI
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(x: 0, y: offset * 10)
+    }
+}
+struct ContentView: View {
+    @State private var cards = [Card]()
+    @Environment(\.accessibilityDifferentiateWithoutColor) var differentiateWithoutColor
+
+    @State private var timeRemaining = 100
+    @Environment(\.scenePhase) var scenePhase
+    @State private var isActive = true
+    @Environment(\.accessibilityVoiceOverEnabled) var voiceOverEnabled
+    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    @State private var showingEditScreen = false
+
+
+    var body: some View {
+        ZStack {
+            Image(decorative: "background")
+                .resizable()
+                .ignoresSafeArea()
+
+            VStack {
+                Text("Time: \(timeRemaining)")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .padding(.horizontal, 20)
+                    .padding(.vertical, 5)
+                    .background(.black.opacity(0.5))
+                    .clipShape(Capsule())
+
+                ZStack {
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index]) {
+                            removeCard(at: index)
+                        }
+                        .stacked(at: index, in: cards.count)
+                        .allowsHitTesting(index == cards.count - 1)
+                        .accessibilityHidden(index < cards.count - 1)
+                    }
+                }
+                .allowsHitTesting(timeRemaining > 0)
+
+                if cards.isEmpty {
+                    Button("Start Again") {
+                        resetCards()
+                    }
+                    .padding()
+                    .background(.black)
+                    .foregroundColor(.white)
+                    .clipShape(Capsule())
+                }
+            }
+
+            VStack {
+                HStack {
+                    Spacer()
+
+                    Button {
+                        showingEditScreen = true
+                    } label: {
+                        Image(systemName: "plus.circle")
+                            .padding()
+                            .background(.black.opacity(0.7))
+                            .clipShape(Circle())
+                    }
+                }
+
+                Spacer()
+            }
+            .foregroundStyle(.white)
+            .font(.largeTitle)
+            .padding()
+
+            if differentiateWithoutColor || voiceOverEnabled {
+                VStack {
+                    Spacer()
+
+                    HStack {
+                        Button {
+                            withAnimation {
+                                removeCard(at: cards.count - 1)
+                            }
+                        } label: {
+                            Image(systemName: "xmark.circle")
+                                .padding()
+                                .background(.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibilityLabel("Wrong")
+                        .accessibilityHint("Mark your answer as incorrect")
+                        Spacer()
+                        Button {
+                            withAnimation {
+                                removeCard(at: cards.count - 1)
+                            }
+                        } label: {
+                            Image(systemName: "checkmark.circle")
+                                .padding()
+                                .background(.black.opacity(0.7))
+                                .clipShape(Circle())
+                        }
+                        .accessibilityLabel("Correct")
+                        .accessibilityHint("Mark your answer as correct")
+                    }
+                    .foregroundStyle(.white)
+                    .font(.largeTitle)
+                    .padding()
+                }
+            }
+        }
+        .onReceive(timer) { time in
+            guard isActive else { return }
+            if timeRemaining > 0 {
+                timeRemaining -= 1
+            }
+        }
+        .onChange(of: scenePhase) { _, newValue in
+            if newValue == .active {
+                if !cards.isEmpty {
+                    isActive = true
+                }
+            } else {
+                isActive = false
+            }
+        }
+        .sheet(isPresented: $showingEditScreen, onDismiss: resetCards, content: EditCards.init)
+        .onAppear(perform: resetCards)
+    }
+
+    func loadData() {
+        if let data = UserDefaults.standard.data(forKey: "Cards") {
+            if let decoded = try? JSONDecoder().decode([Card].self, from: data) {
+                cards = decoded
+            }
+        }
+    }
+
+    func removeCard(at index: Int) {
+        guard index >= 0 else { return }
+        cards.remove(at: index)
+
+        if cards.isEmpty {
+            isActive = false
+        }
+    }
+
+    func resetCards() {
+        loadData()
+        timeRemaining = 100
+        isActive = true
+    }
+
+}
+
+#Preview {
+    ContentView()
+}
